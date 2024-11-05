dd63e4204246508833c318d5f0bc04668ea672bf
zerocat36 <Seo.james2020@gmail.com>
2024-11-05 18:53:32 +0900

    fix(Job.jsx): modified job data rendering logic
    - moved job table rendering function to JobPagination.jsx
    
    - JobPagination.jsx gets filtered job data from Job.jsx, paginates 20 jobs per page and hands over paginated job data to JobTable to render job data

--- src/pages/Job.jsx
+++ src/pages/Job.jsx
@@ -1,59 +1,56 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import MOCK_JobData from "../mock/mock-jobData";
+import React, {useState} from "react";
 import S from "../uis/JobUI";
 import Header from "../components/Header";
 import FilterGroup from "../components/Job/FilterGroup";
-import JobTable from "../components/Job/JobTable";
+import JobPagination from "./JobPagination";
+import MOCK_JobData from "../mock/mock-jobData";
 
 const Job = () => {
-  const [filteredJobs, setFilteredJobs] = useState(MOCK_JobData);
-  const navigate = useNavigate();
-  const handleClick = (id) => navigate(`/job/${id}`);
+    const [filteredJobs, setFilteredJobs] = useState(MOCK_JobData);
 
-  const handleFilterChange = (filters) => {
-    const {
-      selectedWorkTerms,
-      selectedDays,
-      selectedTimes,
-      selectedRegions,
-      selectedOccupations,
-    } = filters;
+    const handleFilterChange = (filters) => {
+        const {
+            selectedWorkTerms,
+            selectedDays,
+            selectedTimes,
+            selectedRegions,
+            selectedOccupations,
+        } = filters;
 
-    const filtered = MOCK_JobData.filter((job) => {
-      const matchesWorkTerms =
-        !selectedWorkTerms.length || selectedWorkTerms.includes(job.workTerm);
-      const matchesDays =
-        !selectedDays.length ||
-        selectedDays.some((day) => job.workDays.includes(day));
-      const matchesTimes =
-        !selectedTimes.length || selectedTimes.includes(job.workTime);
-      const matchesRegions =
-        !selectedRegions.length || selectedRegions.includes(job.location);
-      const matchesOccupations =
-        !selectedOccupations.length ||
-        selectedOccupations.includes(job.workCategory);
+        const filtered = MOCK_JobData && MOCK_JobData.filter((job) => {
+            const matchesWorkTerms =
+                !selectedWorkTerms.length || selectedWorkTerms.includes(job.workTerm);
+            const matchesDays =
+                !selectedDays.length ||
+                selectedDays.some((day) => job.workDays.includes(day));
+            const matchesTimes =
+                !selectedTimes.length || selectedTimes.includes(job.workTime);
+            const matchesRegions =
+                !selectedRegions.length || selectedRegions.includes(job.location);
+            const matchesOccupations =
+                !selectedOccupations.length ||
+                selectedOccupations.includes(job.workCategory);
 
-      return (
-        matchesWorkTerms &&
-        matchesDays &&
-        matchesTimes &&
-        matchesRegions &&
-        matchesOccupations
-      );
-    });
-    setFilteredJobs(filtered);
-  };
+            return (
+                matchesWorkTerms &&
+                matchesDays &&
+                matchesTimes &&
+                matchesRegions &&
+                matchesOccupations
+            );
+        });
+        setFilteredJobs(filtered);
+    };
 
-  return (
-    <>
-      <Header />
-      <S.PageFrame>
-        <FilterGroup onFilterChange={handleFilterChange} />
-        <JobTable jobs={filteredJobs} onRowClick={handleClick} />
-      </S.PageFrame>
-    </>
-  );
+    return (
+        <>
+            <Header/>
+            <S.PageFrame>
+                <FilterGroup onFilterChange={handleFilterChange}/>
+                <JobPagination filteredJobs={filteredJobs}/>
+            </S.PageFrame>
+        </>
+    );
 };
 
 export default Job;
