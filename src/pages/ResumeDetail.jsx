d6e9f70469036cf0da3350e51874a373832cbfeb
zerocat36 <Seo.james2020@gmail.com>
2024-11-15 14:49:56 +0900

    feat(ResumeDetail.jsx): initialize resume detail page

---
+++ src/pages/ResumeDetail.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import MOCK_RESUME from "../mock/mock-resume";
+import Header from "../components/Header";
+import S from "../uis/ResumeUI";
+
+function resumeDetail() {
+
+    return (
+        <>
+            <Header/>
+        </>
+    )
+}
+
+export default resumeDetail;
