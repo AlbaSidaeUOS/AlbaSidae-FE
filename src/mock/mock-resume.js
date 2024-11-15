088ba34aeec1dfb27771e255501b5440cd04fcc4
zerocat36 <Seo.james2020@gmail.com>
2024-11-15 14:37:52 +0900

    feat(mock-resume.js): add resume features, change name
    - add id, title, career, summary, desiredLocation features
    
    - changed name from mock-profile to mock-resume

---
+++ src/mock/mock-resume.js
@@ -0,0 +1,44 @@
+import ProfileImage from "../images/ProfileImage.svg";
+
+const MOCK_RESUME = [
+  {
+    id: 0,
+    title: "이력서",
+    name: "홍길동",
+    gender: "남자",
+    age: "22",
+    career: "경력 3년 8개월 26일",
+    summary: "매장관리/판매, 캐셔/카운터",
+    desiredLocation: "경기 용인시 기흥구",
+    tel: "010-1234-5678",
+    email: "abcd@gmail.com",
+    address: {
+      city: "서울특별시",
+      district: "동대문구",
+      street: "서울시립대로",
+      num: "163",
+    },
+    picture: ProfileImage,
+  },
+  {
+    id: 1,
+    title: "이력서",
+    name: "홍길동",
+    gender: "남자",
+    age: "22",
+    career: "경력 3년 8개월 26일",
+    summary: "매장관리/판매, 캐셔/카운터",
+    desiredLocation: "경기 용인시 기흥구",
+    tel: "010-1234-5678",
+    email: "abcd@gmail.com",
+    address: {
+      city: "서울특별시",
+      district: "동대문구",
+      street: "서울시립대로",
+      num: "163",
+    },
+    picture: ProfileImage,
+  },
+];
+
+export default MOCK_RESUME;
